1ca4d67dd7a01060e061f31a33ef1c6d2de063a6
unknown <zhangs@ZHANGS6460B.onx.com>
2014-01-20 21:49:26 -0500

    Created the project.

---
+++ src/com/bemy/protoMap/MainActivity.java
@@ -0,0 +1,40 @@
+package com.bemy.protoMap;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+	public final static String EXTRA_MESSAGE ="initialize";
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//set full screen
+		requestWindowFeature(Window.FEATURE_NO_TITLE);  		
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		//set title screen
+		//setContentView(R.layout.activity_main);
+		//set play screen
+		ProtoRoom room1=new ProtoRoom(this);
+		setContentView( room1 );
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void createNewGame(View view){
+/*		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		intent.putExtra(EXTRA_MESSAGE, "create a new game!!");
+		startActivity( intent);*/
+		
+		
+	}
+
+}
---
+++ src/com/bemy/protoMap/ProtoRoom.java
@@ -0,0 +1,29 @@
+package com.bemy.protoMap;
+
+import android.view.View;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Canvas;
+
+public class ProtoRoom extends View {
+	private Paint redPaint;
+	private int circleX;
+	private int circleY;
+	private float radius;
+	
+	public ProtoRoom(Context context){
+		super(context);
+		//initialize
+		redPaint=new Paint();
+		redPaint.setColor(Color.RED);
+		circleX=100;
+		circleY=100;
+		radius=50;
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas){
+		canvas.drawCircle( circleX, circleY, radius, redPaint);
+	}
+}
