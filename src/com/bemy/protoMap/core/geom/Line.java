f298cd54fe4f7f9e481f73bc9bd3f960d3ab053d
sophiewjzhang <sophiewjzhang@gmail.com>
2014-06-28 10:32:01 -0400

    Binary file commit.

--- src/com/bemy/protoMap/core/geom/Line.java
+++ src/com/bemy/protoMap/core/geom/Line.java
@@ -10,10 +10,41 @@ public abstract class Line extends shape{
 		this.setPixels();
 	}
 	public void setPixels(int startX,int endX, int startY,int endY){
-		int dx=endX-startX;
-		int dy=endY-startY;
+		int dx=Math.abs(endX-startX);
+		int dy=Math.abs(endY-startY);
+		float slope=dy/dx;
+		int dp=2*dy-dx;
+		int sx=(endX-startX)>0?1:-1;
+		int sy=(endy-starty)>0?1:-1;
 		
-		Vector2f _point=new Vector2f(this.x, this.y);
-		this.pixels.add(_point);
+		this.pixels.add(new Vector2f(0, 0));
+		if( slope < 1){
+			while( startX != endX ){
+				startX += sx;
+				if( dp <0 ){
+				dp +=2*dy;				
+				}else{
+				dp += 2*(dy-dx);
+				starty += sy;
+				}
+				this.pixels.add(new Vector2f(startX, starty));	
+
+			}
+
+		}else{
+			while( starty != endy ){
+				starty += sy;
+				if( dp <0 ){
+				dp +=2*dx;				
+				}else{
+				dp += 2*(dx-dy);
+				startx += sx;
+				}
+				this.pixels.add(new Vector2f(startX, starty));	
+
+			}
+
+		}
+
 	}
 }
