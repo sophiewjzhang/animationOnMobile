914cf39f93797c27481c0ad521f6cce2aa8aaec3
sophiewjzhang <sophiewjzhang@gmail.com>
2014-07-10 14:40:20 -0400

    Revert "Geom Line corrected. Geom Circle added."
    This reverts commit 8adc910f8d13fea6df941c5092b6b221ce04df97.

--- src/com/bemy/protoMap/core/geom/Circle.java
+++ src/com/bemy/protoMap/core/geom/Circle.java
@@ -2,39 +2,18 @@ package com.bemy.protoMap.core.geom;
 
 public class Circle extends shape{
 	private static final long serialVersionUID = 1130000L;
-	private int radius;
-
-	public Circle(int p_radius){
-		this.radius=p_radius;
-		this.setPixels();
-	}
-	public int getRadius(){
-		return this.radius;
+	
+	public Circle(){
+		this.x=0;
+		this.y=0;
+	}	
+	public Circle(int x, int y){
+		this.setPosition(x, y);
 	}
 	@Override
 	public void setPixels() {
-		int startY=0;
-		int dx=radius;
-		int dp=-radius;
-		this.pixels.add(new Vector2f(dx, startY));
-		while( startY <= dx ){
-			startY++;
-			dp +=2*startY+1;
-			if( dp >=0){
-				dx--;
-				dp -=2*dx;
-			}
-			this.pixels.add(new Vector2f(dx, startY));
-			this.pixels.add(new Vector2f(startY, dx));
-			this.pixels.add(new Vector2f(dx, -startY));
-			this.pixels.add(new Vector2f(-startY, dx));
-			this.pixels.add(new Vector2f(-dx, startY));
-			this.pixels.add(new Vector2f(startY, -dx));
-			this.pixels.add(new Vector2f(-dx, -startY));
-			this.pixels.add(new Vector2f(-startY, -dx));
-		}
+		// TODO Auto-generated method stub
 		
 	}
-
 	
 }
--- src/com/bemy/protoMap/core/geom/Line.java
+++ src/com/bemy/protoMap/core/geom/Line.java
@@ -13,14 +13,22 @@ public class Line extends shape{
 		endY=p_endY;
 		this.setPixels();
 	}
+<<<<<<< HEAD
 	@Override
 	public void setPixels() {
+=======
+	public void setPixels(int startX,int endX, int startY,int endY){
+>>>>>>> f298cd54fe4f7f9e481f73bc9bd3f960d3ab053d
 		int dx=Math.abs(endX-startX);
 		int dy=Math.abs(endY-startY);
 		float slope=dy/dx;
 		int dp=2*dy-dx;
 		int sx=(endX-startX)>0?1:-1;
+<<<<<<< HEAD
 		int sy=(endY-startY)>0?1:-1;
+=======
+		int sy=(endy-starty)>0?1:-1;
+>>>>>>> f298cd54fe4f7f9e481f73bc9bd3f960d3ab053d
 		
 		this.pixels.add(new Vector2f(0, 0));
 		if( slope < 1){
@@ -30,22 +38,39 @@ public class Line extends shape{
 				dp +=2*dy;				
 				}else{
 				dp += 2*(dy-dx);
+<<<<<<< HEAD
 				startY += sy;
 				}
 				this.pixels.add(new Vector2f(startX, startY));	
+=======
+				starty += sy;
+				}
+				this.pixels.add(new Vector2f(startX, starty));	
+>>>>>>> f298cd54fe4f7f9e481f73bc9bd3f960d3ab053d
 
 			}
 
 		}else{
+<<<<<<< HEAD
 			while( startY != endY ){
 				startY += sy;
+=======
+			while( starty != endy ){
+				starty += sy;
+>>>>>>> f298cd54fe4f7f9e481f73bc9bd3f960d3ab053d
 				if( dp <0 ){
 				dp +=2*dx;				
 				}else{
 				dp += 2*(dx-dy);
+<<<<<<< HEAD
 				startX += sx;
 				}
 				this.pixels.add(new Vector2f(startX, startY));	
+=======
+				startx += sx;
+				}
+				this.pixels.add(new Vector2f(startX, starty));	
+>>>>>>> f298cd54fe4f7f9e481f73bc9bd3f960d3ab053d
 
 			}
 
