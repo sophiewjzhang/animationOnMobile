8adc910f8d13fea6df941c5092b6b221ce04df97
sophiewjzhang <sophiewjzhang@gmail.com>
2014-07-10 14:33:05 -0400

    Geom Line corrected. Geom Circle added.

--- src/com/bemy/protoMap/core/geom/Circle.java
+++ src/com/bemy/protoMap/core/geom/Circle.java
@@ -2,18 +2,39 @@ package com.bemy.protoMap.core.geom;
 
 public class Circle extends shape{
 	private static final long serialVersionUID = 1130000L;
-	
-	public Circle(){
-		this.x=0;
-		this.y=0;
-	}	
-	public Circle(int x, int y){
-		this.setPosition(x, y);
+	private int radius;
+
+	public Circle(int p_radius){
+		this.radius=p_radius;
+		this.setPixels();
+	}
+	public int getRadius(){
+		return this.radius;
 	}
 	@Override
 	public void setPixels() {
-		// TODO Auto-generated method stub
+		int startY=0;
+		int dx=radius;
+		int dp=-radius;
+		this.pixels.add(new Vector2f(dx, startY));
+		while( startY <= dx ){
+			startY++;
+			dp +=2*startY+1;
+			if( dp >=0){
+				dx--;
+				dp -=2*dx;
+			}
+			this.pixels.add(new Vector2f(dx, startY));
+			this.pixels.add(new Vector2f(startY, dx));
+			this.pixels.add(new Vector2f(dx, -startY));
+			this.pixels.add(new Vector2f(-startY, dx));
+			this.pixels.add(new Vector2f(-dx, startY));
+			this.pixels.add(new Vector2f(startY, -dx));
+			this.pixels.add(new Vector2f(-dx, -startY));
+			this.pixels.add(new Vector2f(-startY, -dx));
+		}
 		
 	}
+
 	
 }
