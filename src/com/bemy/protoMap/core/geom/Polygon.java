19a91198bf290eb68903b1e26d7dfab7eee9767d
Sophie Zhang <sophiewjzhang@gmail.com>
2014-10-01 16:39:33 -0400

    Create polygon class
    Create the polygon class.

--- src/com/bemy/protoMap/core/geom/Polygon.java
+++ src/com/bemy/protoMap/core/geom/Polygon.java
@@ -3,15 +3,22 @@ package com.bemy.protoMap.core.geom;
 public class Polygon extends shape{
 	private static final long serialVersionUID = 1140000L;
 	
-	public Polygon(){
+	public Polygon(int sides){
 		
 	}	
 	public Polygon(int x, int y){
 		this.setPosition(x, y);
 	}
 	@Override
-	public void setPixels() {
+	public void setPixels(int sides, int radius) {
 		// TODO Auto-generated method stub
-		
+		  if( sides <0)return;
+		  var dx,dy;
+		  var n=sides, step=2*Math.PI/n;
+		  for( var i=0;i<n;i++){
+		    dx=radius*Math.round(Math.sin( step*i)*100)/100;
+		    dy=radius*Math.round(Math.cos( step*i)*100)/100;
+		    this.pixels.add(new Vector2f(dx, dy);	
+		  }
 	}
 }
